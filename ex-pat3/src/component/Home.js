d76cbdc63f4645d3bb7ac95d09c26076c2eb0208
Cassandra Dew <cpdew2@gmail.com>
2020-05-28 21:43:08 -0400

    updates

---
+++ ex-pat3/src/component/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home =() => {
+    return(
+        <div>
+            <div className="Home">Home</div>
+        </div>
+    )
+};
+export default Home;
---
+++ ex-pat3/src/component/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Login =() => {
+    return(
+        <div>
+            <div className="Login">Login</div>
+        </div>
+    )
+};
+export default Login;
